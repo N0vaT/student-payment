a859a6c06419e5c4460831382b8386c2e1ec8b8c
N0vaT <Novikovto92@yandex.ru>
2022-07-27 19:35:45 +0300

    first commit

--- src/main/java/com/nova/student/domain/StudentOrder.java
+++ src/main/java/com/nova/student/domain/StudentOrder.java
@@ -1,4 +1,22 @@
 package com.nova.student.domain;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "jc_student_order")
 public class StudentOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_order_id")
+    private Long studentOrderId;
+
+    public Long getStudentOrderId() {
+        return studentOrderId;
+    }
+
+    public void setStudentOrderId(Long studentOrderId) {
+        this.studentOrderId = studentOrderId;
+    }
 }
